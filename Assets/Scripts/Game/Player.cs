f494af862482384088060ea475de4435dc0cd703
ibnbattuta <yoyox98@gmial.com>
2021-01-29 20:46:50 +0100

    Adding simple player movement

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -1,22 +1,22 @@
-﻿using com.romainimberti.ggj2021.game.camera;
-using com.romainimberti.ggj2021.utilities;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace com.romainimberti.ggj2021.game
+namespace com.romainimberti.ggj2020
 {
 
     ///<summary>
-    ///Class that handles the game manager
+    ///
     ///</summary>
-    public class GameManager : SingletonBehaviour<GameManager>
+    public class Player : MonoBehaviour
     {
         #region Variables
         #region Editor
 
         #endregion
         #region Public
+        public float speed;
+        public FloatingJoystick joystick;
 
         #endregion
         #region Private
@@ -26,10 +26,14 @@ namespace com.romainimberti.ggj2021.game
         #region Methods
         #region Unity
 
-
         #endregion
         #region Public
 
+        public void FixedUpdate()
+        {
+            Vector3 direction = Vector3.up * joystick.Vertical + Vector3.right * joystick.Horizontal;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(direction * speed * Time.fixedDeltaTime);
+        }
         #endregion
         #region Protected
 
